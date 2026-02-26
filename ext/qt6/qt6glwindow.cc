b6bb876ef3698c114573de209b77789184eaf7b9
Marc Leeman <marc.leeman@gmail.com>
2026-02-26 10:39:37 +0100

    New upstream version 1.28.1

--- ext/qt6/qt6glwindow.cc
+++ ext/qt6/qt6glwindow.cc
@@ -32,6 +32,7 @@
 
 #include <QtCore/QDateTime>
 #include <QtGui/QGuiApplication>
+#include <QtQuick/QQuickOpenGLUtils>
 #include <QtQuick/QQuickWindow>
 #include <QQuickRenderTarget>
 
@@ -102,7 +103,7 @@ Qt6GLWindow::Qt6GLWindow (QWindow * parent, QQuickWindow *src)
   this->priv->internal_format = GL_RGBA;
 
   connect (source, SIGNAL(beforeRendering()), this, SLOT(beforeRendering()), Qt::DirectConnection);
-  connect (source, SIGNAL(afterFrameEnd()), this, SLOT(afterFrameEnd()), Qt::DirectConnection);
+  connect (source, SIGNAL(afterRendering()), this, SLOT(afterRendering()), Qt::DirectConnection);
   if (source->isSceneGraphInitialized())
     source->scheduleRenderJob(new RenderJob(std::bind(&Qt6GLWindow::onSceneGraphInitialized, this)), QQuickWindow::BeforeSynchronizingStage);
   else
@@ -193,7 +194,7 @@ Qt6GLWindow::beforeRendering()
 }
 
 void
-Qt6GLWindow::afterFrameEnd()
+Qt6GLWindow::afterRendering()
 {
   gboolean ret;
   guint width, height;
@@ -208,6 +209,10 @@ Qt6GLWindow::afterFrameEnd()
     g_mutex_unlock (&this->priv->lock);
     return;
   }
+
+  source->beginExternalCommands();
+
+  QQuickOpenGLUtils::resetOpenGLState();
 
   width = GST_VIDEO_INFO_WIDTH (&this->priv->v_info);
   height = GST_VIDEO_INFO_HEIGHT (&this->priv->v_info);
@@ -281,6 +286,8 @@ Qt6GLWindow::afterFrameEnd()
   GST_DEBUG ("rendering finished");
 
 done:
+  source->endExternalCommands();
+
   gst_gl_context_activate (this->priv->other_context, FALSE);
 
   this->priv->result = ret;
